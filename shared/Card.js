51cca9a8df70c57462afa2fa4dd4f5f20d884d93
Nikhil M Jeby <32586986+solid-droid@users.noreply.github.com>
2021-05-22 15:45:29 +0530

    Revert "Animation for flatlist"
    This reverts commit 5ecb968c69daa41673c2f2c9cec9d6a5a2aeed07.

--- shared/Card.js
+++ shared/Card.js
@@ -1,53 +1,19 @@
 import React from 'react'
-import { StyleSheet, View, Animated, Dimensions} from 'react-native'
-const { height: wHeight } = Dimensions.get("window");
-const height = wHeight - 64;
+import { StyleSheet, View} from 'react-native'
 
-
-export default function Card({children, y , index}) {
-
-    const CARD_HEIGHT = 120
-    const position = Animated.subtract(index * CARD_HEIGHT, y);
-    const isDisappearing = -CARD_HEIGHT;
-    const isTop = 0;
-    const isBottom = height - CARD_HEIGHT;
-    const isAppearing = height;
-    const translateY = Animated.add(
-        Animated.add(
-          y,
-          y.interpolate({
-            inputRange: [0, 0.00001 + index * CARD_HEIGHT],
-            outputRange: [0, -index * CARD_HEIGHT],
-            extrapolateRight: "clamp",
-          })
-        ),
-        position.interpolate({
-          inputRange: [isBottom, isAppearing],
-          outputRange: [0, -CARD_HEIGHT / 4],
-          extrapolate: "clamp",
-        })
-      );
-      const scale = position.interpolate({
-        inputRange: [isDisappearing, isTop, isBottom, isAppearing],
-        outputRange: [0.5, 1, 1, 0.5],
-        extrapolate: "clamp",
-      });
-      const opacity = position.interpolate({
-        inputRange: [isDisappearing, isTop, isBottom, isAppearing],
-        outputRange: [0.5, 1, 1, 0.5],
-      });
+export default function Card(props) {
     return (
-        <Animated.View style={[styles.card,{ opacity, transform: [{ translateY }, { scale }] }]}>
+        <View style={styles.card}>
             <View style={styles.cardComponent}>
-                    {children}
+                    {props.children}
             </View>
-        </Animated.View>
+        </View>
     )
 }
 
 const styles = StyleSheet.create({
     card:{
-        borderRadius:10,
+        borderRadius:6,
         elevation:3,
         backgroundColor:'#fff',
         shadowOffset: {width:1, height:1},
